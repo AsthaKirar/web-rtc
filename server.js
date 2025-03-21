3e800d2086f20ab4ae823b0d230c1191dd3e615d
AsthaKirar <asthap602@gmail.com>
2025-03-21 14:43:26 +0530

    commit

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {createServer} from "http";
+import { Server } from "http";
+import { fileURLToPath } from "url";
+import { dirname,join } from "path";
+
+
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.use(express.static("public"));
+
+
+
+app.get("/",(req,res) =>{
+    console.log("GET Request /")
+    res.sendFile( join( __dirname + "/app/index.html"));
+})
+
+io.on("connection", (socket) => {
+    console.log('server connected to socket server and socket id is ${socket.id}')
+})
+server.listen(9000,()=>{
+    console.log("server is runing on port 9000")
+})
